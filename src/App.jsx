b3bdc95dc426af5966d8ea889e0bc803248ea315
imam maulana <maulanaonwp@yahoo.com>
2017-09-21 13:58:05 +0700

    commite ke 2 atHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import "./App.css"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017'
+    }
+  }
   render () {
     return (
-      <div>
-        <div>Countdownt to December 25, 2017</div>
-        <div>
-          <div>14 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+      <div className="App">
+        <div className="App-title">
+          Countdown to {this.state.deadline}
         </div>
-        <input placeholder='new date'/>
-        <button>Submit</button>
+        <div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date'/>
+          <button>Submit</button>
+        </div>
       </div>
     )
   }
